f6e42b01260d84dace80d3963fca06ee41f256b3
Pratik Dahal <pratikd354@gmail.com>
2025-12-25 09:17:51 +0545

    Final commit

--- script.js
+++ script.js
@@ -1,23 +1,48 @@
 const qrText = document.getElementById('qr-text');
-const Sizes = document.getElementById('Sizes');
-const generate = document.getElementById('generate');
-const download = document.getElementById('download');
-const qrContainer = document.getElementById('.qr-body');
+const sizes = document.getElementById('sizes');
+const generateBtn = document.getElementById('generateBtn');
+const downloadBtn = document.getElementById('downloadBtn');
+const qrContainer = document.querySelector('.qr-body');
 
-let size = Sizes.ariaValueMax;
-
-generateBtn.addEventListener('clock', (e)=>{
+let size = sizes.value;
+generateBtn.addEventListener('click',(e)=>{
     e.preventDefault();
-    generateQrCode();
+    isEmptyInput();
 });
 
-function generateQrCode(){
-    new QRCode(qrContainer, {
-            text: qrText.ariaValueMax,
-            height : size,
-            width : size,
-            colorLight : "#fff",
-            colorDark : "#000"
+sizes.addEventListener('change',(e)=>{
+    size = e.target.value;
+    isEmptyInput();
 });
+
+downloadBtn.addEventListener('click', ()=>{
+    let img = document.querySelector('.qr-body img');
+
+    if(img !== null){
+        let imgAtrr = img.getAttribute('src');
+        downloadBtn.setAttribute("href", imgAtrr);
+    }
+    else{
+        downloadBtn.setAttribute("href", `${document.querySelector('canvas').toDataURL()}`);
+    }
+});
+
+function isEmptyInput(){
+    // if(qrText.value.length > 0){
+    //     generateQRCode();
+    // }
+    // else{
+    //     alert("Enter the text or URL to generate your QR code");
+    // }
+    qrText.value.length > 0 ? generateQRCode() : alert("Enter the text or URL to generate your QR code");;
 }
-
+function generateQRCode(){
+    qrContainer.innerHTML = "";
+    new QRCode(qrContainer, {
+        text:qrText.value,
+        height:size,
+        width:size,
+        colorLight:"#fff",
+        colorDark:"#000",
+    });
+}
